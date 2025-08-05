1fdcf1a529add11de64530bde4c6be3629d1a78d
cetinboran <cetinmesum@gmail.com>
2025-08-05 17:02:42 +0300

    domains room

---
+++ internal/server/domains/rooms/error.go
@@ -0,0 +1,20 @@
+package rooms
+
+const (
+	// Repository Errors
+	ErrFetchFailed    = "rooms.fetch_failed"
+	ErrCountFailed    = "rooms.count_failed"
+	ErrLoginFailed    = "rooms.login_failed"
+	ErrCreateFailed   = "rooms.create_failed"
+	ErrUpdateFailed   = "rooms.update_failed"
+	ErrDeleteFailed   = "rooms.delete_failed"
+	ErrIsExistsFailed = "rooms.exists_check_failed"
+	ErrNotFound       = "rooms.not_found"
+
+	// Domain Errors
+	ErrOwnerIDRequired  = "rooms.owner_id_required"
+	ErrRoomCodeRequired = "rooms.room_code_required"
+
+	// Service Errors
+	ErrRoomCodeAlreadyBeingUsed = "rooms.room_code_already_being_used"
+)
